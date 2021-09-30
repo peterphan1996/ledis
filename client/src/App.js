919b7ba91936859b384b5e50f4a40dbdeed82ae4
Peter Phan <peterphan_1996@live.com>
2021-09-30 18:53:49 +0700

    Improve UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
-import React, { useState, useEffect } from "react";
-import logo from "./logo.svg";
+import React, { useState, useRef, useEffect } from "react";
 import "./App.css";
 
 const App = () => {
   const [inputValue, setInputValue] = useState("");
   const [data, setData] = useState([]);
+
+  const messageEl = useRef(null);
+
+  useEffect(() => {
+    if (messageEl) {
+      messageEl.current.addEventListener("DOMNodeInserted", (event) => {
+        const { currentTarget: target } = event;
+        target.scroll({ top: target.scrollHeight, behavior: "smooth" });
+      });
+    }
+  }, []);
 
   const postCommand = async (command) => {
     const response = await fetch("/", {
@@ -15,14 +25,15 @@ const App = () => {
       body: `command=${command}`,
     });
     const body = await response.json();
+    const data = Array.isArray(body.data) ? body.data : [body.data];
 
     if (response.status !== 200) {
       throw Error(body.message);
     }
-    setData((prev) => [...prev, `> ${command}`, body.data]);
+    setData((prev) => [...prev, `> ${command}`, ...data]);
   };
 
-  const search = (ele) => {
+  const search = () => {
     if (event.key === "Enter") {
       postCommand(inputValue);
       setInputValue("");
@@ -31,20 +42,23 @@ const App = () => {
 
   return (
     <div className="App">
-      <div className="screen">
+      <div className="screen" ref={messageEl}>
         {data.map((info, index) => (
           <p className="screen-text" key={index}>
             {info}
           </p>
         ))}
       </div>
-      <input
-        type="text"
-        className="command-input"
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
-        onKeyDown={search}
-      />
+      <div className="command-container">
+        <p className="prompt">>Ledis: </p>
+        <input
+          type="text"
+          className="command-input"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          onKeyDown={search}
+        />
+      </div>
     </div>
   );
 };
